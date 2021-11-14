bc41526f3224082fe28b6740aa2d111197332c3b
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2021-11-14 10:15:09 +0700

    Rapikan indentasi, gunakan formatter

--- src/tipedata/BilanganBulat.java
+++ src/tipedata/BilanganBulat.java
@@ -2,19 +2,21 @@ package tipedata;
 public class BilanganBulat {
     public static void main(String args[]){
         //deklarasi variabel/state/atribut
-    byte nilaiA;
-    short nilaiB;
-    int hargaA;
-    long hargaB;
-    //inisialisasi/variabel/state/atribut
-    nilaiA =97;
-    nilaiB =30000;
-    hargaA =2000000000;
-    hargaB =3000000000l;
-    //menampilkan nilai dari variabel ke laya output
-    System.out.println(nilaiA);
-    System.out.println(nilaiB);
-    System.out.println(hargaA);
-    System.out.println(hargaB);
+        byte nilaiA;
+        short nilaiB;
+        int hargaA;
+        long hargaB;
+        
+        //inisialisasi/variabel/state/atribut
+        nilaiA = 97;
+        nilaiB = 30000;
+        hargaA = 2000000000;
+        hargaB = 3000000000l;
+        
+        //menampilkan nilai dari variabel ke laya output
+        System.out.println(nilaiA);
+        System.out.println(nilaiB);
+        System.out.println(hargaA);
+        System.out.println(hargaB);
     }
 }
